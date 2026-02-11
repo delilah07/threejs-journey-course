c0bbde8b399d69ead53d401f9f77072299288409
Iryna Abrosimova <abrosimovairyna@gmail.com>
2026-02-11 21:35:28 +0100

    lesson 28

--- lesson-28/dist/assets/index-QGI9U7I5.js
+++ lesson-28/dist/assets/index-QGI9U7I5.js
@@ -4276,6 +4276,45 @@ vec2 rotate(vec2 uv, float rotation, vec2 mid){
     );
 }
 
+vec4 permute(vec4 x){
+    return mod(((x*34.0)+1.0)*x, 289.0);
+}
+
+vec2 fade(vec2 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
+
+float cnoise(vec2 P){
+  vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+  vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+  Pi = mod(Pi, 289.0); 
+  vec4 ix = Pi.xzxz;
+  vec4 iy = Pi.yyww;
+  vec4 fx = Pf.xzxz;
+  vec4 fy = Pf.yyww;
+  vec4 i = permute(permute(ix) + iy);
+  vec4 gx = 2.0 * fract(i * 0.0243902439) - 1.0; 
+  vec4 gy = abs(gx) - 0.5;
+  vec4 tx = floor(gx + 0.5);
+  gx = gx - tx;
+  vec2 g00 = vec2(gx.x,gy.x);
+  vec2 g10 = vec2(gx.y,gy.y);
+  vec2 g01 = vec2(gx.z,gy.z);
+  vec2 g11 = vec2(gx.w,gy.w);
+  vec4 norm = 1.79284291400159 - 0.85373472095314 * 
+    vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
+  g00 *= norm.x;
+  g01 *= norm.y;
+  g10 *= norm.z;
+  g11 *= norm.w;
+  float n00 = dot(g00, vec2(fx.x, fy.x));
+  float n10 = dot(g10, vec2(fx.y, fy.y));
+  float n01 = dot(g01, vec2(fx.z, fy.z));
+  float n11 = dot(g11, vec2(fx.w, fy.w));
+  vec2 fade_xy = fade(Pf.xy);
+  vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+  float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+  return 2.3 * n_xy;
+}
+
 void main()
 {
     
@@ -4459,7 +4498,106 @@ void main()
     
 
     
-    float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - 0.25));
-    gl_FragColor = vec4(vec3(strength), 1.0);
+    
+    
+
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+
+    
+    
+    
+
+    
+    
+    
+
+    
+    
+    
+
+    
+    float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
+    
+
+    
+    vec3 blackColor = vec3(0.0);
+    vec3 uvColor = vec3(vUv, 0.8);
+    vec3 mixedColor = mix(blackColor, uvColor, strength);
+    gl_FragColor = vec4(mixedColor, 1.0);
 }`;new Ys;const Io=document.querySelector("canvas.webgl"),qs=new oc,wp=new Ci(1,1,32,32),Rp=new on({vertexShader:Ap,fragmentShader:bp,side:Ge}),Cp=new Xe(wp,Rp);qs.add(Cp);const ke={width:window.innerWidth,height:window.innerHeight};window.addEventListener("resize",()=>{ke.width=window.innerWidth,ke.height=window.innerHeight,fi.aspect=ke.width/ke.height,fi.updateProjectionMatrix(),Ti.setSize(ke.width,ke.height),Ti.setPixelRatio(Math.min(window.devicePixelRatio,2))});const fi=new Le(75,ke.width/ke.height,.1,100);fi.position.set(.25,-.25,1);qs.add(fi);const No=new tp(fi,Io);No.enableDamping=!0;const Ti=new Jf({canvas:Io});Ti.setSize(ke.width,ke.height);Ti.setPixelRatio(Math.min(window.devicePixelRatio,2));const Fo=()=>{No.update(),Ti.render(qs,fi),window.requestAnimationFrame(Fo)};Fo();
-//# sourceMappingURL=index-bmyQu6uS.js.map
+//# sourceMappingURL=index-QGI9U7I5.js.map
