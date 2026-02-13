a30bf479da8453072b0b90a21feaae5459c10cc5
Iryna Abrosimova <abrosimovairyna@gmail.com>
2026-02-13 15:42:21 +0100

    add partical animation for galaxy

--- lesson-30/src/script.js
+++ lesson-30/src/script.js
@@ -45,26 +45,32 @@ const generateGalaxy = () =>
     const positions = new Float32Array(parameters.count * 3);
     const colors = new Float32Array(parameters.count * 3);
     const scales = new Float32Array(parameters.count * 1);
+    const randomness = new Float32Array(parameters.count * 3);
 
-    const insideColor = new THREE.Color(parameters.insideColor)
-    const outsideColor = new THREE.Color(parameters.outsideColor)
+    const insideColor = new THREE.Color(parameters.insideColor);
+    const outsideColor = new THREE.Color(parameters.outsideColor);
 
     for(let i = 0; i < parameters.count; i++)
     {
-        const i3 = i * 3
+        const i3 = i * 3;
 
         // Position
-        const radius = Math.random() * parameters.radius
+        const radius = Math.random() * parameters.radius;
 
         const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
 
-        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
-        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
-        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
+        positions[i3    ] = Math.cos(branchAngle) * radius;
+        positions[i3 + 1] = 0.0;
+        positions[i3 + 2] = Math.sin(branchAngle) * radius;
 
-        positions[i3    ] = Math.cos(branchAngle) * radius + randomX
-        positions[i3 + 1] = randomY
-        positions[i3 + 2] = Math.sin(branchAngle) * radius + randomZ
+        // Randomness
+        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius;
+        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius;
+        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius;
+
+        randomness[i3    ] = randomX;
+        randomness[i3 + 1] = randomY;
+        randomness[i3 + 2] = randomZ;
 
         // Color
         const mixedColor = insideColor.clone()
@@ -81,6 +87,7 @@ const generateGalaxy = () =>
     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
     geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
     geometry.setAttribute('aScale', new THREE.BufferAttribute(scales, 1))
+    geometry.setAttribute('aRandomness', new THREE.BufferAttribute(randomness, 3))
 
     // Material
     material = new THREE.ShaderMaterial({
@@ -90,6 +97,7 @@ const generateGalaxy = () =>
         vertexShader: galaxyVertexShader,
         fragmentShader: galaxyFragmentShader,
         uniforms: {
+            uTime: {value: 0},
             uSize: {value: 30 * renderer.getPixelRatio()}
         }
     })
@@ -158,6 +166,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // update material
+    material.uniforms.uTime.value = elapsedTime;
 
     // Update controls
     controls.update()
--- lesson-30/src/shaders/galaxy/vertex.glsl
+++ lesson-30/src/shaders/galaxy/vertex.glsl
@@ -1,12 +1,29 @@
 uniform float uSize;
+uniform float uTime;
 
 attribute float aScale;
+attribute vec3 aRandomness;
 
 varying vec3 vColor;
 
 void main(){
     // Position
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    
+    //Spin
+    float angle = atan(modelPosition.x, modelPosition.z);
+    float distanceFromCenter = length(modelPosition.xz);
+    float angleOffset = (1.0 / distanceFromCenter) * uTime * 0.2; 
+    angle += angleOffset;
+    modelPosition.x = cos(angle) * distanceFromCenter;
+    modelPosition.z = sin(angle) * distanceFromCenter;
+
+    // Randomness
+    // modelPosition.x += aRandomness.x;
+    // modelPosition.y += aRandomness.y;
+    // modelPosition.z += aRandomness.z;
+    modelPosition.xyz += aRandomness.xyz;
+
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
