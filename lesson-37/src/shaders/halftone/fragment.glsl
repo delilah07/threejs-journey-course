ad721e4310089cf9211325f54d201c5ebb5b4e3d
Iryna Abrosimova <abrosimovairyna@gmail.com>
2026-05-19 11:25:32 +0200

    rewrite code

--- lesson-37/src/shaders/halftone/fragment.glsl
+++ lesson-37/src/shaders/halftone/fragment.glsl
@@ -8,6 +8,30 @@ varying vec3 vPosition;
 #include ../includes/directionalLight.glsl
 #include ../includes/pointLight.glsl
 
+vec3 halfToneFunction(
+    vec3 color,
+    float repetitions,
+    vec3 direction,
+    float low,
+    float high,
+    vec3 pointColor,
+    vec3 normal
+){
+    float intensity = dot(normal, direction); // how much the grid is visible
+    intensity = smoothstep(low, high, intensity); // smooth the transition between low and high
+
+    vec2 uv = gl_FragCoord.xy / uResolution.y;
+    uv *= repetitions; 
+    uv = mod(uv, 1.0);
+
+    // Discs
+    float point = distance(uv, vec2(0.5));
+    point = 1.0 - step(0.5 * intensity, point); // step(edge, x) returns 0.0 if x < edge, and 1.0 otherwise.
+
+    color = mix(color, pointColor, point); 
+    return color;
+}
+
 void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
@@ -42,18 +66,7 @@ void main()
 
     vec3 pointColor = vec3(1.0, 0.0,0.0); // color of the grid points
 
-    float intensity = dot(normal, direction); // how much the grid is visible
-    intensity = smoothstep(low, high, intensity); // smooth the transition between low and high
-
-    vec2 uv = gl_FragCoord.xy / uResolution.y;
-    uv *= repetitions; 
-    uv = mod(uv, 1.0);
-
-    // Discs
-    float point = distance(uv, vec2(0.5));
-    point = 1.0 - step(0.5 * intensity, point); // step(edge, x) returns 0.0 if x < edge, and 1.0 otherwise.
-
-    color = mix(color, pointColor, point); 
+    color = halfToneFunction(color, repetitions, direction, low, high, pointColor, normal);
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
