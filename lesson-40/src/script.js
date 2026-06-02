c3b2521ca72d18f5eedb88ea1de3e866ebba255e
Iryna Abrosimova <abrosimovairyna@gmail.com>
2026-06-02 11:08:02 +0200

    add animation between 2 models

--- lesson-40/src/script.js
+++ lesson-40/src/script.js
@@ -129,6 +129,7 @@ gltfLoader.load('./models.glb', (gltf) => {
   // Geometry
   particles.geometry = new THREE.BufferGeometry();
   particles.geometry.setAttribute('position', particles.positions[1]);
+  particles.geometry.setAttribute('aPositionTarget', particles.positions[3]);
 
   // Material
   particles.material = new THREE.ShaderMaterial({
@@ -142,6 +143,7 @@ gltfLoader.load('./models.glb', (gltf) => {
           sizes.height * sizes.pixelRatio,
         ),
       ),
+      uProgress: new THREE.Uniform(0),
     },
     blending: THREE.AdditiveBlending,
     depthWrite: false,
@@ -150,6 +152,14 @@ gltfLoader.load('./models.glb', (gltf) => {
   // Points
   particles.points = new THREE.Points(particles.geometry, particles.material);
   scene.add(particles.points);
+
+  // Tweaks
+  gui
+    .add(particles.material.uniforms.uProgress, 'value')
+    .min(0)
+    .max(1)
+    .step(0.001)
+    .name('uProgress');
 });
 
 // Animate
