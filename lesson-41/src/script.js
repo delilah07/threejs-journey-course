6cc8b0e5e5a9e8464ba53f2a5aa920c1a30dc145
Iryna Abrosimova <abrosimovairyna@gmail.com>
2026-06-12 12:40:22 +0200

    use the GPGPU to position particles

--- lesson-41/src/script.js
+++ lesson-41/src/script.js
@@ -114,7 +114,7 @@ for (let i = 0; i < baseGeometry.count; i++) {
 
 // Particles variables
 gpgpu.particlesVariable = gpgpu.computation.addVariable(
-  'uParticlesTexture',
+  'uParticles',
   gpgpuParticlesFragmentShader,
   baseParticlesTexture,
 );
@@ -141,6 +141,30 @@ console.log;
 // Particles
 const particles = {};
 
+// Geometry
+const particlesUvArray = new Float32Array(baseGeometry.count * 2);
+
+for (let y = 0; y < gpgpu.size; y++) {
+  for (let x = 0; x < gpgpu.size; x++) {
+    const i = y * gpgpu.size + x;
+    const i2 = i * 2;
+
+    // Particles UV
+    const uvX = (x + 0.5) / gpgpu.size;
+    const uvY = (y + 0.5) / gpgpu.size;
+
+    particlesUvArray[i2 + 0] = uvX;
+    particlesUvArray[i2 + 1] = uvY;
+  }
+}
+
+particles.geometry = new THREE.BufferGeometry();
+particles.geometry.setDrawRange(0, baseGeometry.count);
+particles.geometry.setAttribute(
+  'aParticlesUv',
+  new THREE.BufferAttribute(particlesUvArray, 2),
+);
+
 // Material
 particles.material = new THREE.ShaderMaterial({
   vertexShader: particlesVertexShader,
@@ -153,11 +177,12 @@ particles.material = new THREE.ShaderMaterial({
         sizes.height * sizes.pixelRatio,
       ),
     ),
+    uParticlesTexture: new THREE.Uniform(),
   },
 });
 
 // Points
-particles.points = new THREE.Points(baseGeometry.instance, particles.material);
+particles.points = new THREE.Points(particles.geometry, particles.material);
 scene.add(particles.points);
 
 // Tweaks
@@ -185,6 +210,8 @@ const tick = () => {
 
   // GPGPU update
   gpgpu.computation.compute();
+  particles.material.uniforms.uParticlesTexture.value =
+    gpgpu.computation.getCurrentRenderTarget(gpgpu.particlesVariable).texture;
 
   // Render normal scene
   renderer.render(scene, camera);
